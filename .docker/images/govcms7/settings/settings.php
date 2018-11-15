58a60018f7f1cb1a8627809e93d901688f8492f5
Stuart Rowlands <srowlands@users.noreply.github.com>
2018-11-15 14:29:22 +1000

    Cloud/Local SFP support (#42)
    * Enable SFP in all non-prod environments.
    * Set stage file proxy origin if env variable is set.
    * Support for cloud + local.

--- .docker/images/govcms7/settings/settings.php
+++ .docker/images/govcms7/settings/settings.php
@@ -187,6 +187,15 @@ if (file_exists(__DIR__ . '/settings.local.php')) {
 }
 
 // Stage file proxy URL from production URL.
-if (getenv('STAGE_FILE_PROXY_URL')) {
-  $conf['stage_file_proxy_origin'] = getenv('STAGE_FILE_PROXY_URL');
+if (getenv('LAGOON_ENVIRONMENT_TYPE') != 'production') {
+
+  if (getenv('LAGOON_PROJECT')) {
+    $origin = 'https://nginx-' . getenv('LAGOON_PROJECT') . '-master.govcms.amazee.io';
+    $conf['stage_file_proxy_origin'] = $origin;
+  }
+
+  if (getenv('STAGE_FILE_PROXY_URL')) {
+    $conf['stage_file_proxy_origin'] = getenv('STAGE_FILE_PROXY_URL');
+  }
+
 }
