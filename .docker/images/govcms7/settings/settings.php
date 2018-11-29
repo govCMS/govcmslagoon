70bcb9e5fc0ea0c5edb495d3b5298c56cae1d7af
Thom Toogood <thomtoogood@gmail.com>
2018-11-29 15:47:17 +1100

    Added system/files to fast_404_string_whitelisting.

--- .docker/images/govcms7/settings/settings.php
+++ .docker/images/govcms7/settings/settings.php
@@ -25,7 +25,7 @@ $contrib_path = 'sites/all/modules/contrib';
 include_once($contrib_path . '/fast_404/fast_404.inc');
 $conf['fast_404_exts'] = '/^(?!robots)^(?!sites\/default\/files\/private).*\.(?:png|gif|jpe?g|svg|tiff|bmp|raw|webp|docx?|xlsx?|pptx?|swf|flv|cgi|dll|exe|nsf|cfm|ttf|bat|pl|asp|ics|rtf)$/i';
 $conf['fast_404_html'] = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN" "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd"><html xmlns="http://www.w3.org/1999/xhtml"><head><title>404 Not Found</title></head><body><h1>Not Found</h1><p>The requested URL "@path" was not found on this server.</p></body></html>';
-$conf['fast_404_string_whitelisting'] = array('robots.txt');
+$conf['fast_404_string_whitelisting'] = array('robots.txt', 'system/files');
 
 // Allow custom themes to provide custom 404 pages.
 // By placing a file called 404.html in the root of their theme repository.
