0fecc38520edc921b73aed13ac9100fb30b7c021
Joseph Z <pandaskii@users.noreply.github.com>
2018-11-30 11:15:10 +1100

    Add page cache settings to production.settings.php

--- .docker/images/govcms7/settings/production.settings.php
+++ .docker/images/govcms7/settings/production.settings.php
@@ -6,5 +6,16 @@
  * This file will only be included on production environments.
  */
 
+// Cache settings.
+if (!class_exists('DrupalFakeCache')) {
+  $conf['cache_backends'][] = 'includes/cache-install.inc';
+}
+// Rely on the external cache for page caching.
+$conf['cache_class_cache_page'] = 'DrupalFakeCache';
+$conf['cache'] = 1;
+$conf['page_cache_maximum_age'] = 300;
+// We can't use an external cache if we are trying to invoke these hooks.
+$conf['page_cache_invoke_hooks'] = FALSE;
+
 // Inject Google Analytics snippet on all production sites.
 $conf['googleanalytics_codesnippet_after'] = "ga('create', 'UA-54970022-1', 'auto', {'name': 'govcms'}); ga('govcms.send', 'pageview', {'anonymizeIp': true})";
