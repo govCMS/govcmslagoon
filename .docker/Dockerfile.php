6e90357b91e894e939fccc917b41b1dbe63eeab7
Toby Bellwood <toby.bellwood@finance.gov.au>
2019-03-11 17:16:07 +1100

    Track upstream images and tidyup

--- .docker/Dockerfile.php
+++ .docker/Dockerfile.php
@@ -1,11 +1,13 @@
 ARG CLI_IMAGE
+ARG LAGOON_IMAGE_VERSION
+ARG PHP_IMAGE_VERSION
 FROM ${CLI_IMAGE} as cli
 
-FROM amazeeio/php:7.1-fpm
+FROM amazeeio/php:${PHP_IMAGE_VERSION}-fpm-${LAGOON_IMAGE_VERSION}
 
-# Temporary override until lagoon PR is available in upstream image.
-# https://github.com/amazeeio/lagoon/issues/787
-ENV PHP_MAX_INPUT_VARS=2000
+RUN apk add gmp gmp-dev \
+    && docker-php-ext-install gmp \
+    && docker-php-ext-configure gmp
 
 RUN apk add --update clamav clamav-libunrar \
     && freshclam
