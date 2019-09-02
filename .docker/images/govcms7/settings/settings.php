a5cf99f2af5dd028d38fbe4dbd1157cbee030dde
Steve Worley <sj.worley88@gmail.com>
2019-09-02 16:24:21 +1000

    Add a catch for MARIADB_HOST_READREPLICA.

--- .docker/images/govcms7/settings/settings.php
+++ .docker/images/govcms7/settings/settings.php
@@ -118,17 +118,19 @@ if (getenv('LAGOON')) {
     'collation' => 'utf8mb4_general_ci',
   ];
 
-  $databases['read']['default'] = [
-    'driver' => 'mysql',
-    'database' => getenv('MARIADB_DATABASE') ?: 'drupal',
-    'username' => getenv('MARIADB_USERNAME') ?: 'drupal',
-    'password' => getenv('MARIADB_PASSWORD') ?: 'drupal',
-    'host' => getenv('MARIADB_HOST_READREPLICA') ?: 'mariadb',
-    'host' => getenv('MARIADB_HOST') ?: 'mariadb',
-    'port' => 3306,
-    'charset' => 'utf8mb4',
-    'collation' => 'utf8mb4_general_ci',
-  ];
+  if (getenv('MARIADB_HOST_READREPLICA')) {
+    $databases['read']['default'] = [
+      'driver' => 'mysql',
+      'database' => getenv('MARIADB_DATABASE') ?: 'drupal',
+      'username' => getenv('MARIADB_USERNAME') ?: 'drupal',
+      'password' => getenv('MARIADB_PASSWORD') ?: 'drupal',
+      'host' => getenv('MARIADB_HOST_READREPLICA') ?: 'mariadb',
+      'host' => getenv('MARIADB_HOST') ?: 'mariadb',
+      'port' => 3306,
+      'charset' => 'utf8mb4',
+      'collation' => 'utf8mb4_general_ci',
+    ];
+  }
 }
 
 // Lagoon Solr connection
