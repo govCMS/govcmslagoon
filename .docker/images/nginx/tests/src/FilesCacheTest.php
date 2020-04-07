ef5c08904b5e59abdd7c57b3d48305f1d7ca2638
Alex Skrypnyk <alex@integratedexperts.com>
2020-04-07 18:51:05 +1000

    Updated test helpers and fixed coding standards.

--- .docker/images/nginx/tests/src/FilesCacheTest.php
+++ .docker/images/nginx/tests/src/FilesCacheTest.php
@@ -7,37 +7,37 @@ use PHPUnit\Framework\TestCase;
 class FilesCacheTest extends TestCase {
 
   /**
-   * Return a list of files to test.
-   *
-   * @return array
-   *   File list.
-   */
-  public function filesProvider() {
-    $path = dirname(__DIR__);
-    return [
-      ["autotest.jpg", "$path/resources/", 'max-age=2628001'],
-      ["autotest.pdf", "$path/resources/", 'max-age=1800'],
-      ["autotest.rtf", "$path/resources/", 'max-age=2628001'],
-    ];
-  }
-
-  /**
    * {@inheritdoc}
    */
   public function setUp(): void {
     `docker-compose exec nginx mkdir -p /app/sites/default/files`;
-    foreach ($this->filesProvider() as $parts) {
+    foreach ($this->providerExpiredHeaderPath() as $parts) {
       list($file, $path) = $parts;
       `docker cp $path/$file $(docker-compose ps -q nginx):/app/sites/default/files/`;
     }
   }
 
   /**
+   * Return a list of files to test.
+   *
+   * @return array
+   *   File list.
+   */
+  public function providerExpiredHeaderPath() {
+    $path = dirname(__DIR__);
+    return [
+      ['autotest.jpg', "$path/resources/", 'max-age=2628001'],
+      ['autotest.pdf', "$path/resources/", 'max-age=1800'],
+      ['autotest.rtf', "$path/resources/", 'max-age=2628001'],
+    ];
+  }
+
+  /**
    * Ensure that expires headers are correctly set.
    *
-   * @dataProvider filesProvider
+   * @dataProvider providerExpiredHeaderPath
    */
-  public function testHeader($file, $path, $expected) {
+  public function testExpiredHeaderPath($file, $path, $expected) {
     $path = "/sites/default/files/$file";
     $headers = \get_curl_headers($path);
     $this->assertEquals($expected, $headers['Cache-Control']);
